0677ec552bdec8fc356dd99c2bdf0d2cc956c325
damian <dfauth@gmail.com>
2020-02-05 07:25:04 +0800

    add authorization call to route implementation

--- rest/src/main/scala/com/github/dfauth/auth/Routes.scala
+++ rest/src/main/scala/com/github/dfauth/auth/Routes.scala
@@ -2,11 +2,17 @@ package com.github.dfauth.auth
 
 import java.security.KeyPair
 
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import com.github.dfauth.jwt.{JWTBuilder, JWTVerifier, KeyPairFactory}
 import com.github.dfauth.auth.Directives._
+import com.github.dfauth.authzn.{AuthorizationPolicyImpl, AuthorizationPolicyMonadImpl}
+import com.github.dfauth.authzn
+import com.github.dfauth.authzn.PrincipalType._
+import com.github.dfauth.authzn
 import com.typesafe.scalalogging.LazyLogging
+
+import scala.util.{Failure, Success}
 
 object Routes extends LazyLogging {
 
@@ -14,15 +20,25 @@ object Routes extends LazyLogging {
   val keyPair: KeyPair = KeyPairFactory.createKeyPair("RSA", 2048)
   val jwtBuilder = new JWTBuilder(issuer,keyPair.getPrivate)
   val jwtVerifier = new JWTVerifier(keyPair.getPublic, issuer)
+  val policy = AuthorizationPolicyMonadImpl(new authzn.Directive(ROLE.of("user")))
+  val permission = new TestPermission()
 
 
   def hello(jwtVerifier: JWTVerifier) =
     path("hello") {
       get {
         authenticate(jwtVerifier) { userCtx =>
-          complete(HttpEntity(ContentTypes.`application/json`, s"""{"say": "hello to authenticated ${userCtx.userId()}"}"""))
+          policy.permit(userCtx.payload(), permission) {
+            // extract username
+            userCtx.userId()
+          } match {
+            case Success(s) => complete(HttpEntity(ContentTypes.`application/json`, s"""{"say": "hello to authenticated ${s}"}"""))
+            case Failure(t) => complete(StatusCodes.Unauthorized)
+          }
         }
       }
     }
 
 }
+
+class TestPermission() extends authzn.Permission
