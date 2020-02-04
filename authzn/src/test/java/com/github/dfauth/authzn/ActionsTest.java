e523a54e2cc228f6cf76fe1760e92b947c446e03
fauthd <damian.fauth@sgx.com>
2020-02-04 12:59:57 +0800

    more Action tests

--- authzn/src/test/java/com/github/dfauth/authzn/ActionsTest.java
+++ authzn/src/test/java/com/github/dfauth/authzn/ActionsTest.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 import static com.github.dfauth.authzn.ActionSet.ALL_ACTIONS;
+import static com.github.dfauth.authzn.ActionsTest.TestAction.*;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
@@ -17,19 +18,45 @@ public class ActionsTest {
 
         {
             ActionSet actionSet = ActionSet.parse("create, read, update, delete");
-            assertTrue(ALL_ACTIONS.implies(TestAction.CREATE));
-            assertTrue(actionSet.implies(TestAction.CREATE));
-            assertFalse(TestAction.CREATE.implies(ALL_ACTIONS));
+            assertTrue(ALL_ACTIONS.implies(CREATE));
+            assertTrue(actionSet.implies(CREATE));
+            assertFalse(CREATE.implies(ALL_ACTIONS));
             assertFalse(actionSet.implies(ALL_ACTIONS));
+            assertFalse(CREATE.implies(READ));
+            assertTrue(READ.implies(READ));
+        }
+
+        {
+            ActionSet actionSet = ActionSet.parse("create, read, update, delete");
+            assertTrue(ALL_ACTIONS.implies(TestAction2.CREATE));
+            assertTrue(actionSet.implies(TestAction2.CREATE));
+            assertFalse(TestAction2.CREATE.implies(ALL_ACTIONS));
+            assertFalse(actionSet.implies(ALL_ACTIONS));
+            assertFalse(TestAction2.CREATE.implies(TestAction2.READ));
+            assertTrue(TestAction2.READ.implies(TestAction2.CREATE));
+            assertTrue(TestAction2.READ.implies(TestAction2.READ));
         }
 
     }
 
-    private enum TestAction implements Action {
+    enum TestAction implements Action {
         CREATE, READ, UPDATE, DELETE;
 
         @Override
         public boolean implies(Action action) {
+            return this == action;
+        }
+    }
+
+    enum TestAction2 implements Action {
+        CREATE, READ, UPDATE, DELETE;
+
+        @Override
+        public boolean implies(Action action) {
+            if(action instanceof TestAction2) {
+                TestAction2 action2 = (TestAction2) action;
+                return ordinal() >= action2.ordinal();
+            }
             return this == action;
         }
     }
