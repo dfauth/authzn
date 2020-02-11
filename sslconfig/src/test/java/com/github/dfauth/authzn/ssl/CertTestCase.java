661191bbc8cc6ee49636212dc294f62fc944df5f
damian <dfauth@gmail.com>
2020-02-12 05:08:11 +0800

    certgen plugin working

--- sslconfig/src/test/java/com/github/dfauth/authzn/ssl/CertTestCase.java
+++ sslconfig/src/test/java/com/github/dfauth/authzn/ssl/CertTestCase.java
@@ -5,12 +5,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.*;
 import java.security.cert.CertificateException;
 import java.security.spec.InvalidKeySpecException;
+
+import static org.testng.Assert.assertTrue;
 
 public class CertTestCase {
 
@@ -19,7 +22,8 @@ public class CertTestCase {
     @Test
     public void testIt() {
         try {
-            new X509CertificateGenerator("server.CA-signed.p12", "password", "1", false).createCertificate("127.0.0.1", 365, "testAlias", new FileOutputStream("src/main/resources/test.p12"), "password");
+            new X509CertificateGenerator("server.CA-signed.p12", "password", "1", false).createCertificate("127.0.0.1", 365, "testAlias", new FileOutputStream("target/test.p12"), "password");
+            assertTrue(new File("target/test.p12").exists());
         } catch (InvalidKeyException e) {
             logger.error(e.getMessage(), e);
             throw new RuntimeException(e);
